e8b83e003d13612164d5914e0f33c925cc8e42d2
ChinmayRaj <chinmayshah2002@gmail.com>
2025-05-31 12:10:55 +0530

    form validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -11,14 +12,24 @@ import { FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
 export class AppComponent {
 
  profileForm=new FormGroup({
-  name:new FormControl(),
-  password:new FormControl(),
-  email:new FormControl()
+  name:new FormControl('',[Validators.required]),
+  password:new FormControl('',[Validators.minLength(5),Validators.required]),
+  email:new FormControl('',[Validators.required,Validators.maxLength(50),Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')])
  });
 
  onSubmit(){
   console.log("Submit button clicked , Value is :"+this.profileForm.value);
   
  }
+ get name(){
+  return this.profileForm.get('name');
+ }
+ get email(){
+  return this.profileForm.get('email');
+ }
+ get password(){
+  return this.profileForm.get('password');
+ }
+
 
 }
